0c3900a706ab71f27b81abad9f06f46ac1c18fab
Vikas Bishnoi <vikaskbm@gmail.com>
2021-04-05 09:55:09 +0530

    getting data and prop drilling it to cards

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import styles from './App.module.css';
 import {fetchData} from './api';
 
 class App extends React.Component {
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const data = await fetchData();
+        this.setState({ data: data })
+    }
+
     render() {
+        const {data} = this.state;
         return (
             <div className={styles.container}>
-                <Cards/>
+                <Cards data={data} />
                 <Chart/>
                 <CountryPicker/>
             </div>
