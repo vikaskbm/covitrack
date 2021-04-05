1d90cb3bb35fc8baca20ad08e7102e3ed6693139
Vikas Bishnoi <vikaskbm@gmail.com>
2021-04-05 13:43:55 +0530

    prop drill data to Chart component

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <Cards data={data} />
-                <Chart/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart/>
             </div>
         )
     }
