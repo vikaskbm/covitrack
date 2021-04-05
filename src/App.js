c7c662db4a0cbd1029ec629afd6692f8afb5c2d8
Vikas Bishnoi <vikaskbm@gmail.com>
2021-04-05 08:42:34 +0530

    export App component and add css file for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>coviTrack</h1>
+                <Cards/>
+                <Chart/>
+                <CountryPicker/>
             </div>
         )
     }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
 import App from './App';
 
