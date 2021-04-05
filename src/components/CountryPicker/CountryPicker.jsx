37cc54cdb274e27892672aa5edd80558d79bf07c
Vikas Bishnoi <vikaskbm@gmail.com>
2021-04-05 13:32:41 +0530

    fetch data from api into web app

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -5,13 +5,23 @@ import { fetchCountries } from '../../api';
 import styles from './CountryPicker.module.css';
 
 const CountryPicker = () => {
+    const [countries, setCountries] = useState([]);
+    
+    useEffect(() => {
+    const fetchAPI = async () => {
+        setCountries(await fetchCountries());
+        };
+
+        fetchAPI();
+    }, []);
+    
     return (
-    <FormControl className={styles.formControl}>
-      <NativeSelect defaultValue="">
-        <option value="">Global</option>
-      </NativeSelect>
-    </FormControl>
-  );
+        <FormControl className={styles.formControl}>
+        <NativeSelect defaultValue="">
+            <option value="">Global</option>
+        </NativeSelect>
+        </FormControl>
+    );
 };
 
 export default CountryPicker;
