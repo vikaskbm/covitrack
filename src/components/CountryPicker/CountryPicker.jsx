344cc4e28ef2f0be3b413312a2616c69ba265567
Vikas Bishnoi <vikaskbm@gmail.com>
2021-04-05 13:29:43 +0530

    add form control to select countries

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
 
 const CountryPicker = () => {
-    return <h1>CountryPicker</h1>
-}
+    return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue="">
+        <option value="">Global</option>
+      </NativeSelect>
+    </FormControl>
+  );
+};
 
 export default CountryPicker;
